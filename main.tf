63356719f788fcf623637f87862f0f73fcbcd614
nilesh <nileshaqua@gmail.com>
2024-02-01 08:51:15 +0530

    provider block added

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=3.0.1"
+      version = "3.89.0"
     }
   }
+}
+
+provider "azurerm" {
+  # Configuration options
 }
