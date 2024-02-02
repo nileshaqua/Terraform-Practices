c2114026b0e24264d0cfee81d72c491baf8932f0
nilesh <nileshaqua@gmail.com>
2024-02-02 11:52:33 +0530

    duplicate resource block deleted

--- main.tf
+++ main.tf
@@ -17,8 +17,3 @@ resource "azurerm_resource_group" "nileshrg1" {
   name     = "nileshrg1"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "nileshrg1" {
-  name     = "nileshrg1"
-  location = "West Europe"
-}
