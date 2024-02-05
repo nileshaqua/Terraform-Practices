050f1551b317ec333616a0779591db8b51969beb
nilesh <nileshaqua@gmail.com>
2024-02-05 12:22:03 +0530

    varibale added in main.tf

--- main.tf
+++ main.tf
@@ -13,19 +13,7 @@ provider "azurerm" {
   }
 }
 
-<<<<<<< HEAD
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.location
-  }
-=======
-resource "azurerm_resource_group" "nileshrg1" {
-  name     = "nileshrg1"
-  location = "West Europe"
 }
-
-resource "azurerm_resource_group" "nileshrg1" {
-  name     = "nileshrg1"
-  location = "West Europe"
-}
->>>>>>> parent of c211402 (duplicate resource block deleted)
